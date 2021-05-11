9b6fc007fe97657b4eab72995f587a06ad319875
MikkelYtting <Mytting1996@gmail.com>
2021-05-11 11:02:53 +0200

    Initial commit

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,51 @@
+﻿using CommentSection;
+using Microsoft.AspNetCore.Mvc;
+using SemProFeedBack.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SemProFeedBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        // GET: CommentController
+        private CommentManager _CommentManager = new CommentManager();
+        [HttpGet]
+        public IEnumerable<Comment> Get()
+        {
+            return _CommentManager.getAll();
+        }
+
+        // GET: CommentController/Details/5
+        [HttpGet("{id}")]
+        public Comment Get(int id)
+        {
+            return _CommentManager.GetById(id);
+        }
+
+        [HttpPost]
+        public Comment Post([FromBody] Comment value)
+        {
+            return _CommentManager.Add(value);
+        }
+        // PUT api/<CommentController>/5
+        [HttpPut("{id}")]
+        public Comment Put(int id, [FromBody] Comment value)
+        {
+            return _CommentManager.Update(id, value);
+        }
+        // DELETE api/<CommentController>/5
+        [HttpDelete("{id}")]
+        public Comment Delete(int id)
+        {
+            return _CommentManager.Delete(id);
+        }
+
+    }
+}
---
+++ Manager/CommentManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommentSection;
+
+namespace SemProFeedBack.Manager
+{
+    public class CommentManager
+    {
+        private static int _nextId = 1;
+
+        public static readonly List<Comment> CommentData = new List<Comment>
+        {
+            new Comment {Id = _nextId++, Commenttext = "123"}
+        };
+
+        public List<Comment> getAll()
+        {
+            return CommentData;
+        }
+
+        public Comment GetById(int id)
+        {
+            return CommentData.Find(comment => comment.Id == id);
+        }
+
+        public Comment Add(Comment newComment)
+        {
+            newComment.Id = _nextId++;
+            CommentData.Add(newComment);
+            return newComment;
+        }
+
+        public Comment Delete(int id)
+        {
+            var comment = CommentData.Find(song1 => song1.Id == id);
+            if (comment == null) return null;
+            CommentData.Remove(comment);
+            return comment;
+        }
+
+        public Comment Update(int id, Comment updates)
+        {
+            var Comment = CommentData.Find(Comment1 => Comment1.Id == id);
+            if (Comment == null) return null;
+            Comment.Commenttext = updates.Commenttext;
+
+            return Comment;
+        }
+    }
+}
